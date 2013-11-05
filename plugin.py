03526b18b4b4f56749f8c4a29670427653368753
Sijis Aviles <saviles@gogoair.com>
2013-11-05 16:21:37 -0600

    initial import

---
+++ plugin.py
@@ -0,0 +1,24 @@
+###
+# Copyright (c) 2013, Sijis Aviles
+# All rights reserved.
+#
+#
+###
+
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+
+
+class Zendesk(callbacks.Plugin):
+    """Add the help for "@plugin help Zendesk" here
+    This should describe *how* to use this plugin."""
+    threaded = True
+
+
+Class = Zendesk
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
---
+++ test.py
@@ -0,0 +1,14 @@
+###
+# Copyright (c) 2013, Sijis Aviles
+# All rights reserved.
+#
+#
+###
+
+from supybot.test import *
+
+class ZendeskTestCase(PluginTestCase):
+    plugins = ('Zendesk',)
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
