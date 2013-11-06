021dc21e3d58ce7659b75a3c338b88e76a012414
Sijis Aviles <saviles@gogoair.com>
2013-11-06 15:01:19 -0600

    adding basic functionality to plugin - added registry keys to hold api info - added zendesk method to pull ticket info - requires: python-requests

--- config.py
+++ config.py
@@ -16,8 +16,28 @@ def configure(advanced):
     from supybot.questions import expect, anything, something, yn
     conf.registerPlugin('Zendesk', True)
 
+    user = something('What is your api username?')
+    password = something('What is your password for that username?')
+    api_url = something('What is your api url (eg. https://<instance>.zendesk.com/api/v2)?')
+    url = something('What is your domain (eg. https://<instance>.zendesk.com)?')
+
+    conf.supybot.plugins.Zendesk.apiUser.setValue(user)
+    conf.supybot.plugins.Zendesk.apiPass.setValue(password)
+    conf.supybot.plugins.Zendesk.apiUrl.setValue(api_url)
+    conf.supybot.plugins.Zendesk.domain.setValue(url)
 
 Zendesk = conf.registerPlugin('Zendesk')
+conf.registerGlobalValue(Zendesk, 'apiUser',
+    registry.String('', """Defines the username used to access the API."""))
+conf.registerGlobalValue(Zendesk, 'apiPass',
+    registry.String('', """Defines the password used to access the API."""))
+conf.registerGlobalValue(Zendesk, 'apiUrl',
+    registry.String('', """Defines the full base api url to access the API.
+    (eg. https://<instance>.zendesk.com/api/v2)"""))
+conf.registerGlobalValue(Zendesk, 'domain',
+    registry.String('', """Defines the url to access the website.
+    (eg. https://<instance>.zendesk.com)"""))
+
 # This is where your configuration variables (if any) should go.  For example:
 # conf.registerGlobalValue(Zendesk, 'someConfigVariableName',
 #     registry.Boolean(False, """Help for someConfigVariableName."""))
--- plugin.py
+++ plugin.py
@@ -11,12 +11,42 @@ import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 
+import requests
 
 class Zendesk(callbacks.Plugin):
     """Add the help for "@plugin help Zendesk" here
     This should describe *how* to use this plugin."""
     threaded = True
 
+    def __init__(self, irc):
+        self.__parent = super(Zendesk, self)
+        self.__parent.__init__(irc)
+        self.user = self.registryValue('apiUser')
+        self.password = self.registryValue('apiPass')
+        self.url = self.registryValue('apiUrl')
+        self.domain = self.registryValue('domain')
+        
+    def zendesk(self, irc, msg, args, id):
+        """<id>
+
+        Returns the subject of the ticket along with a link to it.
+        """
+
+        url = '%s/tickets/%s.json' % (self.url, id)
+        display_url = '%s/tickets/%s' % (self.domain, id)
+        req = requests.get(url, auth=(self.user, self.password))
+        data = req.json()
+
+        user = self._get_name_by_id(data['ticket']['assignee_id'])
+        irc.reply(str('%s created on %s by %s - %s' % 
+                (data['ticket']['subject'], data['ticket']['created_at'], user, display_url)))
+    zendesk = wrap(zendesk, ['int'])
+
+    def _get_name_by_id(self, id):
+        url = '%s/users/%s.json' % (self.url, id)
+        req = requests.get(url, auth=(self.user, self.password))
+        data = req.json()
+        return data['user']['name']
 
 Class = Zendesk
 
